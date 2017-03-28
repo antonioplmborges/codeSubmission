b6959078422d27a290affb87e9462daff7cfaeb6
Antonio Borges <apborges@umass.edu>
2017-03-27 22:22:39 -0400

    Initial Commit

---
+++ currencyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  currencyConverter
+//
+//  Created by Antonio Borges on 3/27/17.
+//  Copyright © 2017 Antonio Borges. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
